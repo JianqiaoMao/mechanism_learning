a25c96bc39b194077aa6cf24b3fb1178eafdf6b3
Jianqiao Mao <maojianqiao001@gmail.com>
2024-06-05 18:00:21 +0100

    Update mechanism_learning.py

--- mechanism_learning.py
+++ mechanism_learning.py
@@ -3,6 +3,32 @@ import causalBootstrapping as cb
 import numpy as np
 #%%
 def mechanism_classifier(cause_data, mediator_data, effect_data, dist_map, ml_model, rebalance = False, n_samples = None, cb_mode = "fast"):
+    """
+    This function trains a machine learning model to predict the effect variable given the cause variable and the mediator variable.
+    
+    Parameters:
+    cause_data: dict
+        A dictionary containing the cause variable data. The key is the variable name and the value is the data.
+    mediator_data: dict
+        A dictionary containing the mediator variable data. The key is the variable name and the value is the data.
+    effect_data: dict   
+        A dictionary containing the effect variable data. The key is the variable name and the value is the data.
+    dist_map: dict
+        A dictionary containing the distribution functions. The key is the variable name and the value is the distribution function.
+    ml_model: object
+        A machine learning model object. It should have the fit method.
+    rebalance: bool
+        If True, the data will be rebalanced by the cause variable.
+    n_samples: int
+        The number of samples to be generated for each value of the cause variable. If None, the number of samples will be the same for each value of the cause variable.
+    cb_mode: str
+        The mode of causal bootstrapping. It can be "fast" or "robust".
+    
+    Returns:
+    ml_model: object
+        The trained machine learning model.
+    """
+    
     cause_var_name = list(cause_data.keys())[0]
     effect_var_name = list(effect_data.keys())[0]
     if rebalance:
